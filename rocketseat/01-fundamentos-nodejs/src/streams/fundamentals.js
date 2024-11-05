c54e892bcb6d669be7e66cee8aec5ac9f66460fc
Jefferson Ferreira <jefferson.ferreira@mjv.com.br>
2024-11-05 16:19:10 -0300

    Commit: Streams no Node.js - Criando stream de escrita e transformação

--- rocketseat/01-fundamentos-nodejs/src/streams/fundamentals.js
+++ rocketseat/01-fundamentos-nodejs/src/streams/fundamentals.js
@@ -1,4 +1,5 @@
-import { Readable } from 'node:stream'
+import { Readable, Writable, Transform } from 'node:stream'
+
 
 class OneToHundredStream extends Readable {
     index = 1;
@@ -19,4 +20,20 @@ class OneToHundredStream extends Readable {
     }
 }
 
-new OneToHundredStream().pipe(process.stdout)
+class InverseNumberStream extends Transform {
+    _transform(chunk, encoding, callback) {
+        const transformed = Number(chunk.toString()) * -1;
+        callback(null, Buffer.from(String(transformed)));
+    }
+}
+
+class MultiplyByTenStream extends Writable {
+    _write(chunk, encoding, callback) {
+        console.log(Number(chunk.toString()) * 10)
+        callback()
+    }
+}
+
+new OneToHundredStream()
+    .pipe(new InverseNumberStream())
+    .pipe(new MultiplyByTenStream())
