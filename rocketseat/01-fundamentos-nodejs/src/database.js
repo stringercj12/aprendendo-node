2bfe7441f5e54a0623cf8bd1e5918a74c04a9bbe
Jefferson Ferreira <jefferson.ferreira@mjv.com.br>
2024-11-07 14:55:56 -0300

    Commit: Rotas da aplicação - Finalizando as rotas

--- rocketseat/01-fundamentos-nodejs/src/database.js
+++ rocketseat/01-fundamentos-nodejs/src/database.js
@@ -18,9 +18,17 @@ export class Database {
         fs.writeFile(databasePath, JSON.stringify(this.#database))
     }
 
-    select(table) {
+    select(table, search) {
         const data = this.#database[table] ?? []
 
+        if (search) {
+            data = data.filter(row => {
+                return Object.entries(search).some(([key, value]) => {
+                    return row[key].toLowerCase().includes(value.toLowerCase())
+                })
+            })
+        }
+        
         return data;
     }
 
@@ -34,5 +42,25 @@ export class Database {
         this.#persist();
 
         return data;
+    }
+
+    update(table, id, data) {
+        const rowIndex = this.#database[table].findIndex(row => row.id === id)
+
+        if (rowIndex > -1) {
+            this.#database[table][rowIndex] = { id, ...data }
+            this.#persist()
+        }
+
+    }
+
+    delete(table, id) {
+        const rowIndex = this.#database[table].findIndex(row => row.id === id)
+
+        if (rowIndex > -1) {
+            this.#database[table].splice(rowIndex, 1)
+            this.#persist()
+        }
+
     }
 }
--- rocketseat/01-fundamentos-nodejs/src/routes.js
+++ rocketseat/01-fundamentos-nodejs/src/routes.js
@@ -1,5 +1,6 @@
 import { Database } from "./database.js";
 import { randomUUID } from 'node:crypto'
+import { buildRoutePath } from "./utils/build-route-path.js";
 
 
 const database = new Database();
@@ -7,16 +8,21 @@ const database = new Database();
 export const routes = [
     {
         method: 'GET',
-        path: '/users',
+        path: buildRoutePath('/users'),
         handler: (req, res) => {
-            const users = database.select('users')
+            const { search } = req.query
+
+            const users = database.select('users', search ? {
+                name: search,
+                email: search
+            } : null)
 
             return res.end(JSON.stringify(users));
         }
     },
     {
         method: 'POST',
-        path: '/users',
+        path: buildRoutePath('/users'),
         handler: (req, res) => {
             const { name, email } = req.body;
             const user = {
@@ -28,6 +34,32 @@ export const routes = [
             database.insert('users', user)
 
             return res.writeHead(201).end();
+        }
+    },
+    {
+        method: 'UPDATE',
+        path: buildRoutePath('/users/:id'),
+        handler: (req, res) => {
+            const { id } = req.params
+            const { name, email } = req.body
+
+            database.update('users', id, {
+                name,
+                email
+            })
+
+            return res.writeHead(204).end()
+        }
+    },
+    {
+        method: 'DELETE',
+        path: buildRoutePath('/users/:id'),
+        handler: (req, res) => {
+            const { id } = req.params
+
+            database.delete('users', id)
+
+            return res.writeHead(204).end()
         }
     }
 ]
